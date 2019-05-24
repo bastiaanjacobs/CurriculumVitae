f27a54451a02c59c00efca9c00d3e8f67b326463
unknown <bas-jacobs@hotmail.com>
2019-05-24 13:56:53 +0200

    added readme file

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,21 @@ new Vue({
   el: "#vue-app",
   data: {
     name: "bastiaan",
-    surname: "jacobs"
+    surname: "jacobs",
+    title: "Front-end Developer & Product Specialist",
+    summary:
+      "Web Developer en SharePoint branding specialist. \
+        Ontwerp- en technologie-enthousiast. \
+        Duiker en gefascineerd door het zeeleven. \
+        Houdt ervan nieuwe plekken te verkennen. Animal avenger.",
+    twitter: "https://twitter.com/bastiaanjacobs",
+    github: "https://www.github.com/bastiaanjacobs",
+    linkedin: "https://www.linkedin.com/in/bastiaanjacobs",
+    website: "https://bastiaanjacobs.nl"
+  },
+  methods: {
+    greet: function(time) {
+      return "Good" + time + " " + this.name;
+    }
   }
 });
